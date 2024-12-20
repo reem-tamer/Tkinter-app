069f1cd4c5b7c884c62e29c0d80613933fc5576e
NoraNader <noranader1372@gmail.com>
2024-12-20 16:52:45 +0200

    r

--- App.py
+++ App.py
@@ -1,2 +1,16 @@
-print("hello")
-#hiii
+import tkinter as tk
+from tkinter import messagebox
+
+class Pet :
+    def __init__(self,name,age,species):
+        self.name =name
+        self.age =age
+        self.species =species
+        self.vaccination_rec = []
+    def adding_vac_rec(self,vaccine):
+        self.vaccination_rec.append(vaccine)
+
+
+
+class Receptionist :
+    def __init__(self):
